154ac2cdb7b60c918d3fa5bf9ed1c84a9be79672
Matt Giguere <matthew.giguere@gmail.com>
2015-05-15 13:23:33 -0400

    update and document startup script

--- vumpsc.sh
+++ vumpsc.sh
@@ -1,6 +1,7 @@
 #!/bin/csh
 
-#SETUP IDL ENVIRONMENT                                                                                   
+#########################################################                                                
+# SETUP IDL ENVIRONMENT                                                                                   
 #########################################################                                                
 if (-e /Applications/exelis) then
 set IDLDIR='/Applications/exelis/idl'
@@ -10,8 +11,16 @@ endif
 
 setenv IDL_STARTUP ~/.idl_startup.pro
 
-setenv IDL_PATH +${HOME}/projects/idlutils:+${MDIR}projects/IDLAstro:+${IDLDIR}/lib
-setenv IDL_PATH +${HOME}/projects/IDLAstro:${IDL_PATH}
+# ADD DEPENDENCIES TO PATH:
+# 1st and 2nd dependencies: IDLAstro Package and the built in IDL lib:
+# https://github.com/wlandsman/IDLAstro
+setenv IDL_PATH +${HOME}/projects/IDLAstro/pro:+${IDLDIR}/lib
+
+# 3rd dependency: idlutils
+# https://github.com/mattgiguere/idlutils
+setenv IDL_PATH +${HOME}/projects/idlutils:${IDL_PATH}
+
+# Now add the recursive path to the VUMPS commissioning code:
 setenv IDL_PATH +${HOME}/projects/VUMPS/COMMISSIONING/code:${IDL_PATH}
 
 set PROPATH = ${HOME}'/projects/VUMPS/COMMISSIONING/code'
